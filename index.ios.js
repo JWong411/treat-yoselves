bac549eddf9786a709e812acde5cddc531ca0f11
[01;31m[K"name"[m[K: "Jason Wong <[01;31m[K"email"[m[K: "jason.wong411@gmail.com>
2016-10-28 12:55:20 -0400

    successfully retrieve polylines based on dropped pins

--- index.ios.js
+++ index.ios.js
@@ -25,19 +25,13 @@ export default class AwesomeProject extends Component {
         longitudeDelta: 0.0421,
       },
       markers: [
-        {
-        latlng: {
-          latitude: 37.78825,
-          longitude: -122.4324
-        },
-        title: "Bullshit marker",
-        description: "It's bullshit man."
-        }
+
       ]
     }
     this.onRegionChange = this.onRegionChange.bind(this);
     this.handlePress = this.handlePress.bind(this);
     this.buttonClicked = this.buttonClicked.bind(this);
+    this.createURL = this.createURL.bind(this);
   }
 
   onRegionChange(region) {
@@ -45,14 +39,51 @@ export default class AwesomeProject extends Component {
   }
 
   handlePress(info) {
-    console.log(info.nativeEvent.coordinate);
+    // console.log(info.nativeEvent.coordinate);
     var lat = info.nativeEvent.coordinate.latitude
     var lng = info.nativeEvent.coordinate.longitude
     this.setState({markers:[...this.state.markers, {latlng: {latitude: lat, longitude: lng}, title: "added some shit", description: "ye ye ye"}]  })
   }
 
+
   buttonClicked() {
     console.log("this worked");
+    var url = this.createURL();
+    var url2 = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyBF4ErRr95eWjXbhMKD5ot1kmmetfa2Umk"
+    var url3 = "https://facebook.github.io/react-native/movies.json"
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        var stepsAry = []
+        responseJson.routes[0].legs.forEach(leg => {
+          leg.steps.forEach(step => stepsAry.push(step))
+        })
+        // console.log(stepsAry);
+        var polylines = stepsAry.map(step => step.polyline.points)
+        console.log(polylines);
+
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
+  createURL() {
+    var origin = this.state.markers[0].latlng.latitude + "," + this.state.markers[0].latlng.longitude
+    var destination = this.state.markers[this.state.markers.length-1].latlng.latitude + "," +  this.state.markers[this.state.markers.length-1].latlng.longitude
+    var waypointsAry = []
+    this.state.markers.forEach((marker, index) => {
+      if (!(index === 0 || index === this.state.markers.length-1)) {
+        waypointsAry.push(marker.latlng.latitude + "," + marker.latlng.longitude)
+      }
+    })
+    var waypoints = waypointsAry.join('|')
+
+    var url = ["https://maps.googleapis.com/maps/api/directions/json?origin=", origin, "&destination=", destination, "&waypoints=", waypoints, "&key=AIzaSyBF4ErRr95eWjXbhMKD5ot1kmmetfa2Umk"].join('');
+    // "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination" + destination + "&waypoints" + waypoints + "&key=AIzaSyBF4ErRr95eWjXbhMKD5ot1kmmetfa2Umk"
+    return url;
   }
 
   render() {
@@ -73,10 +104,13 @@ export default class AwesomeProject extends Component {
                   />
                 ))}
               </MapView>
-              <TouchableHighlight
-                onPress = {this.buttonClicked}>
-                  <Text>Get Routes</Text>
-              </TouchableHighlight>
+              <View
+                style = {styles.button}>
+                <TouchableHighlight
+                  onPress = {this.buttonClicked}>
+                    <Text>Get Routes</Text>
+                </TouchableHighlight>
+              </View>
           </View>
     );
   }
@@ -105,7 +139,10 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 30,
   },
-
+  button: {
+    position: 'absolute',
+    bottom: 0,
+  },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
