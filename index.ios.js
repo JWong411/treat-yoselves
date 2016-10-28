3ea91ec4870a540a68f20e9388950877943352a3
[01;31m[K"name"[m[K: "Jason Wong <[01;31m[K"email"[m[K: "jason.wong411@gmail.com>
2016-10-27 20:04:12 -0400

    initial commit. pins on map

---
+++ index.ios.js
@@ -0,0 +1,116 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class AwesomeProject extends Component {
+  constructor(){
+    super();
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      markers: [
+        {
+        latlng: {
+          latitude: 37.78825,
+          longitude: -122.4324
+        },
+        title: "Bullshit marker",
+        description: "It's bullshit man."
+        }
+      ]
+    }
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
+  // getInitialState() {
+  //   return {
+  //     region: {
+  //       latitude: 37.78825,
+  //       longitude: -122.4324,
+  //       latitudeDelta: 0.0922,
+  //       longitudeDelta: 0.0421,
+  //     },
+  //   };
+  // }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+        // <div id="map"></div>
+        // <View>
+          // <MapView
+          // style={ styles.map }
+          //   initialRegion={{
+          //     latitude: 37.78825,
+          //     longitude: -122.4324,
+          //     latitudeDelta: 0.0922,
+          //     longitudeDelta: 0.0421,
+          //   }}
+          // />
+          <MapView
+            style={ styles.map }
+            region={this.state.region}
+            onRegionChange={this.onRegionChange}
+          >
+            {this.state.markers.map(marker => (
+              <MapView.Marker
+                coordinate={marker.latlng}
+                title={marker.title}
+                description={marker.description}
+              />
+            ))}
+          </MapView>
+          // <Text>Test</Text>
+        // </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
